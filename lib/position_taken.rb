23d8e93a07c21c0bc0f5167a6050af407124ba5c
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 00:54:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,11 @@
-# code your #position_taken? method here!
 def position_taken?(board, index)
-  if board= "X"
-    return true
-    elsif board= "O"
-    return true
-    elsif board== ""
-    return false
-    elsif board== nil
-    return false
-  end
-end
+  if board[index] == " "
+ return false
+ elsif board[index] == ""
+ 	return false
+ elsif board[index] == nil
+ 	return false
+ else
+ 	return true
+ end
+ 	 end
